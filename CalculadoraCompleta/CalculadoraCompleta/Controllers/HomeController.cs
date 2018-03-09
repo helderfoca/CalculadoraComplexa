3e5455d073594ddec9cdad88bcb71160dede8cd4
Helder Lopes <aluno19784@ipt.pt>
2018-03-09 17:06:26 +0000

    Criação da Interface Gráfica

---
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraCompleta.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet] //facultativo, usado por defeito
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt, string display)
+        {
+            return View();
+        }
+    }
+}
