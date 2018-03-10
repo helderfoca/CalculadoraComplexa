483d020e5e212ba8a8d3256c39c5222cb975fefc
Helder Lopes <aluno19784@ipt.pt>
2018-03-10 16:18:41 +0000

    Calculadora funcional sem validação de entrada/erros

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -19,6 +19,59 @@ namespace CalculadoraCompleta.Controllers
         [HttpPost]
         public ActionResult Index(string bt, string display)
         {
+            int pos;
+            float num1, num2, resultado;
+            if (bt == "C")
+            {
+                display = "";
+            }
+            else if (bt == "+/-")
+            {
+                num1 = (float)Convert.ToDecimal(display);
+                resultado = num1 * -1;
+                display = resultado.ToString();
+            }
+            else if (bt == "=")
+            {
+                if (display.Contains('+'))
+                {
+                    pos = display.IndexOf('+');
+                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
+                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
+                    resultado = num1 + num2;
+                }
+                else if (display.Contains('-'))
+                {
+                    pos = display.IndexOf('-');
+                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
+                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
+                    resultado = num1 - num2;
+                }
+                else if (display.Contains('x'))
+                {
+                    pos = display.IndexOf('x');
+                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
+                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
+                    resultado = num1 * num2;
+                }
+                else if (display.Contains(':'))
+                {
+                    pos = display.IndexOf(':');
+                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
+                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
+                    resultado = num1 / num2;
+                }
+                else
+                {
+                    resultado = (float)Convert.ToDecimal(display);
+                }
+                display = resultado.ToString();
+            }
+            else
+            {
+                display += bt;
+            }
+            ViewBag.Display = display;
             return View();
         }
     }
