f3b095e01953bc57fb50ab28cb2ee344f957a99c
Helder Lopes <aluno19784@ipt.pt>
2018-03-16 16:50:54 +0000

    calculadora com código e css da aula

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -8,10 +8,20 @@ namespace CalculadoraCompleta.Controllers
 {
     public class HomeController : Controller
     {
+
+        // esta forma não funciona...
+        // bool primeiroOperador = true;
+
+
         // GET: Home
-        [HttpGet] //facultativo, usado por defeito
+        [HttpGet] // facultativo, pq por defeito é sempre este o verbo utilizado
         public ActionResult Index()
         {
+            // inicialização dos primeiros valores da calculadora
+            Session["primeiroOperador"] = true;
+            Session["iniciaOperando"] = true;
+            ViewBag.Display = "0";
+
             return View();
         }
 
@@ -19,59 +29,98 @@ namespace CalculadoraCompleta.Controllers
         [HttpPost]
         public ActionResult Index(string bt, string display)
         {
-            int pos;
-            float num1, num2, resultado;
-            if (bt == "C")
+
+            // avaliar o valor atribuído à variável 'bt'
+            switch (bt)
             {
-                display = "";
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    if ((bool)Session["iniciaOperando"] ||
+                         display.Equals("0")) display = bt;
+                    else display += bt;
+                    Session["iniciaOperando"] = false;
+                    break;
+
+                case "+/-":
+                    display = Convert.ToDouble(display) * -1 + "";
+                    break;
+
+                case ",":
+                    if (!display.Contains(",")) display += ",";
+                    break;
+
+                case "+":
+                case "-":
+                case "x":
+                case ":":
+                case "=":
+                    // se NÃO é a primeira vez que carrego num operador
+                    if (!(bool)Session["primeiroOperador"])
+                    {
+                        // recuperar os valores dos operandos
+                        double operando1 =
+                            Convert.ToDouble((string)Session["primeiroOperando"]);
+                        double operando2 = Convert.ToDouble(display);
+
+                        switch ((string)Session["operadorAnterior"])
+                        {
+                            case "+":
+                                display = operando1 + operando2 + "";
+                                break;
+                            case "-":
+                                display = operando1 - operando2 + "";
+                                break;
+                            case "x":
+                                display = operando1 * operando2 + "";
+                                break;
+                            case ":":
+                                display = operando1 / operando2 + "";
+                                break;
+                        } //  switch((string)Session["operadorAnterior"])
+                    } // if
+                      // guardar os dados do display para utilização futura
+                      // guardar o valor do 1º operando
+                    Session["primeiroOperando"] = display;
+                    // limpar display
+                    Session["iniciaOperando"] = true;
+
+                    if (bt.Equals("="))
+                    {
+                        // marcar o operador como primeiro operador
+                        Session["primeiroOperador"] = true;
+                    }
+                    else
+                    {
+                        // guardar o valor do operador
+                        Session["operadorAnterior"] = bt;
+                        Session["primeiroOperador"] = false;
+                    }
+
+                    // marcar o display para reinício
+                    Session["iniciaOperando"] = true;
+
+                    break;
+
+                case "C":
+                    // reiniciar a calculadora
+                    Session["iniciaOperando"] = true;
+                    Session["primeiroOperador"] = true;
+                    display = "0";
+                    break;
             }
-            else if (bt == "+/-")
-            {
-                num1 = (float)Convert.ToDecimal(display);
-                resultado = num1 * -1;
-                display = resultado.ToString();
-            }
-            else if (bt == "=")
-            {
-                if (display.Contains('+'))
-                {
-                    pos = display.IndexOf('+');
-                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
-                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
-                    resultado = num1 + num2;
-                }
-                else if (display.Contains('-'))
-                {
-                    pos = display.IndexOf('-');
-                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
-                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
-                    resultado = num1 - num2;
-                }
-                else if (display.Contains('x'))
-                {
-                    pos = display.IndexOf('x');
-                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
-                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
-                    resultado = num1 * num2;
-                }
-                else if (display.Contains(':'))
-                {
-                    pos = display.IndexOf(':');
-                    num1 = (float)Convert.ToDecimal(display.Substring(0, pos));
-                    num2 = (float)Convert.ToDecimal(display.Substring(pos + 1));
-                    resultado = num1 / num2;
-                }
-                else
-                {
-                    resultado = (float)Convert.ToDecimal(display);
-                }
-                display = resultado.ToString();
-            }
-            else
-            {
-                display += bt;
-            }
+
+            // preparar os dados para erem enviados para a View
             ViewBag.Display = display;
+
+
             return View();
         }
     }
